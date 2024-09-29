ef988262c137dea863dc6f2bc9b5a3c33a88227c
SrPanda420 <alexander.redes2002@gmail.com>
2024-09-29 17:46:29 -0500

    Part1

---
+++ part1/src/App.jsx
@@ -0,0 +1,51 @@
+const Header = (porps) => {
+  return (
+    <h1> {porps.course} </h1>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part partName={props.part1} exercises={props.exercises1} />
+      <Part partName={props.part2} exercises={props.exercises2} />
+      <Part partName={props.part3} exercises={props.exercises3} />
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Total de Ejercicios: {props.totalExercises} </p>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p> {props.partName}: {props.exercises} </p>
+  )
+}
+ 
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course}/>
+      <Content 
+       part1={part1} exercises1={exercises1}
+       part2={part2} exercises2={exercises2}
+       part3={part3} exercises3={exercises3}
+      />
+      <Total totalExercises={exercises1 + exercises2 + exercises3} />
+    </div>
+  )
+}
+
+export default App
